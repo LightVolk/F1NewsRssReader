bc26389272dd7b8991f8445ba4cc8d4bf8b63be8
Simplevolk <simplevolk@gmail.com>
2016-09-01 22:12:55 +0300

    try to add share button

--- app/src/main/java/maleev/konstantin/f1newsrssreader/PageActivity.java
+++ app/src/main/java/maleev/konstantin/f1newsrssreader/PageActivity.java
@@ -6,10 +6,15 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
+import android.widget.ShareActionProvider;
 
 public class PageActivity extends AppCompatActivity {
+
+    private ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,8 @@ public class PageActivity extends AppCompatActivity {
         setContentView(R.layout.page_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
 
         WebView webView= (WebView) findViewById(R.id.webViewPage);
 
@@ -50,4 +57,23 @@ public class PageActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate menu resource file.
+        getMenuInflater().inflate(R.menu.share_menu, menu);
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+
+        // Fetch and store ShareActionProvider
+        mShareActionProvider = (ShareActionProvider) item.getActionProvider();
+        //return super.onCreateOptionsMenu(menu);
+        return true;
+    }
+
+    // Call to update the share intent
+    private void setShareIntent(Intent shareIntent) {
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(shareIntent);
+        }
+    }
 }
