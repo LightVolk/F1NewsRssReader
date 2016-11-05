526e2bc802e95f97d443c19c1f1f882bffe2fc47
Simplevolk <simplevolk@gmail.com>
2016-11-05 22:14:46 +0300

    возврат к 1 варианту

---
+++ app/src/main/java/maleev/konstantin/f1newsrssreader/Adapters/NewsOnClickListenew.java
@@ -0,0 +1,14 @@
+package maleev.konstantin.f1newsrssreader.Adapters;
+
+import android.view.View;
+
+/**
+ * Created by Simplevolk on 16.10.2016.
+ */
+
+public class NewsOnClickListenew implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+        
+    }
+}
--- app/src/main/java/maleev/konstantin/f1newsrssreader/MainActivityFragment.java
+++ app/src/main/java/maleev/konstantin/f1newsrssreader/MainActivityFragment.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,8 +20,6 @@ import com.crazyhitty.chdev.ks.rssmanager.RssReader;
 import java.util.ArrayList;
 import java.util.List;
 
-import maleev.konstantin.f1newsrssreader.Adapters.MyAdapter;
-
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -31,9 +27,6 @@ public class MainActivityFragment extends Fragment {
 
 
 
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
 
     public MainActivityFragment() {
     }
@@ -44,40 +37,28 @@ public class MainActivityFragment extends Fragment {
         View view=inflater.inflate(R.layout.fragment_main, container, false);
 
         final RssLoader loader=new RssLoader(getContext(),getActivity());
-        //loader.loadFeeds();
+        loader.loadFeeds();
 
-        RecyclerView listView= (RecyclerView) view.findViewById(R.id.listViewNews);
+        ListView listView= (ListView) view.findViewById(R.id.listViewNews);
 
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        mRecyclerView.setHasFixedSize(true);
-
-        // use a linear layout manager
-        mLayoutManager = new LinearLayoutManager(getActivity());
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-        ArrayList<String> myDataset=new ArrayList<>();
-        myDataset=loader.loadFeedsArr();
-        // specify an adapter (see also next example)
-        mAdapter = new MyAdapter((String[]) myDataset.toArray());
-        mRecyclerView.setAdapter(mAdapter);
-
-        listView.setOnClickListener(new View.OnClickListener() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+
                 if(loader.getRssItems().isEmpty())
                 {
                     return;
                 }
 
-               // RssItem item=loader.getRssItems().get(v);
-              //  if(item==null)
-              //      return;
+                RssItem item=loader.getRssItems().get(i);
+                if(item==null)
+                    return;
 
                 //start new activity with text of news
                 Intent launchBrowser = new Intent(getActivity(), PageActivity.class);
-              //  String goUrl=item.getLink();
-              //  launchBrowser.putExtra("url", goUrl);
+                String goUrl=item.getLink();
+                launchBrowser.putExtra("url", goUrl);
 
                 startActivity(launchBrowser);
             }
